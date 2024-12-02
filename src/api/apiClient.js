533d3adc7c0c4661b76b493666f0947611b7b11f
root <root@e2e-101-241.ssdcloudindia.net>
2024-12-02 19:18:33 +0530

    server changesneeded

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { getToken } from "../utils/storage";
 
-export const BASE_URL = "http://localhost:1337/api";
-export const MEDIA_BASE_URL = "http://localhost:1337";
+// export const BASE_URL = "http://localhost:1337/api";
+// export const MEDIA_BASE_URL = "http://localhost:1337";
 
+
+export const BASE_URL = "https://api.allerpal.com/api";
+export const MEDIA_BASE_URL = "https://api.allerpal.com";
 const apiClient = axios.create({
   baseURL: BASE_URL,
   headers: {
